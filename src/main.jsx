ee431f9222514aaa83c3aea5a1db434536ceff47
Siddharth <rambosid1401@gmail.com>
2024-11-10 17:33:33 +0530

    new check

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Make sure this is correct
 
 
 createRoot(document.getElementById('root')).render(
